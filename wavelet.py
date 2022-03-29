4c24445e31a6509fd56cbdffabd0388c54a4465a
John Crown <80222381+JohnCrown11@users.noreply.github.com>
2022-03-29 14:49:29 -0700

    Update wavelet.py

--- wavelet.py
+++ wavelet.py
@@ -1,31 +1,29 @@
 import numpy as np
 import scipy
 import math
-
+from scipy import integrate
+import sympy as sym
 e = math.e
-
+j=[0, 1]
+k=[0, -1, 1]
 def phi(A, t, s, c):
     if c:
         return (A*e**((-t**2)/2)) * (e ** (1j * s * t)) - (e ** ((-s**2)/2))
     return (A*e**((-t**2)/2)) * (e ** (-1j * s * t)) - (e ** ((-s**2)/2))
 
 def inner_product(func, j, k):
-    return 2**(j/2) * scipy.integrate.quad(phi(1, x*2**j, 1, 0)*func, 0, 1)
+    x=sym.Symbol('x')
+    return 2**(j/2) * sym.integrate(phi(1, x*2**j, 1, 0)*func, (x, 0, 1))
 
 
 def testfunct():
+    x=sym.Symbol('x')
     return x**2
-
-inner_product(testfunct, 0, 0)
-
-
-
-
-
-
-
-
-
-
-
-
+def runExpansion(arrJ, arrK):
+    currsum = 0
+    x=sym.Symbol('x')
+    for p in range(len(arrJ)):
+        for q in range(len(arrK)):
+            currsum += 2**(arrJ[p]/2)*phi(1, x*2**arrJ[p], 1, 1)*inner_product(testfunct(), arrJ[p], arrK[q])
+    return currsum
+print(runExpansion(j, k))
