a98346794b4bdb22e40c2a700d222771e1fa0dd8
John Crown <80222381+JohnCrown11@users.noreply.github.com>
2022-03-31 19:52:58 -0700

    Update wavelet.py

--- wavelet.py
+++ wavelet.py
@@ -7,16 +7,18 @@ from sympy.plotting import plot
 import cmath
 e = math.e
 j=[0]
-k=[0]
+k=[0, 1]
 def phi(A, t, s, c):
+    print("done")
     if c:
         return (A*e**((-t**2)/2)) * (e ** (1j * s * t)) - (e ** ((-s**2)/2))
     return (A*e**((-t**2)/2)) * (e ** (-1j * s * t)) - (e ** ((-s**2)/2))
 
 def inner_product(func, j, k):
     x=sym.Symbol('x')
-    return 2**(j/2) * sym.integrate(phi(1, x*2**j, 1, 0)*func, (x, 0, 1))
-
+    ret =  2**(j/2) * sym.integrate(phi(1, x*2**j-k, 1, 0)*func, (x, 0, 1))
+    print("half")
+    return ret
 
 def testfunct():
     x=sym.Symbol('x')
@@ -26,7 +28,8 @@ def runExpansion(arrJ, arrK):
     x=sym.Symbol('x')
     for p in range(len(arrJ)):
         for q in range(len(arrK)):
-            currsum += 2**(arrJ[p]/2)*phi(1, x*2**arrJ[p], 1, 1)*inner_product(testfunct(), arrJ[p], arrK[q])
+            print(q)
+            currsum += 2**(arrJ[p]/2)*phi(1, x*2**arrJ[p]-arrK[q], 1, 1)*inner_product(testfunct(), arrJ[p], arrK[q])
     return currsum
 def rSq(functionInp, functionTest):
     ar1 = []
@@ -36,15 +39,15 @@ def rSq(functionInp, functionTest):
         ar2.append(functionTest.evalf(subs={'x': i}))
     currErr = 0
     for i in range(len(ar1)):
-        print(ar1[i])
-        print(ar2[i])
+        #print(ar1[i])
+        #print(ar2[i])
         currErr += (ar2[i]-ar1[i])**2
-    print(currErr)
+    #print(currErr)
     return cmath.sqrt((currErr/len(ar1)))
-print(runExpansion(j, k))
-p1 = plot(sym.re(runExpansion(j, k)), show = False)
-#p2 = plot(testfunct, show = False)
-#p1.append(p2[0])
+#print(runExpansion(j, k))
+exp = runExpansion(j, k)
+print(rSq(exp, testfunct()))
+p1 = plot(sym.re(exp), show = False)
+p2 = plot(testfunct(), show = False)
+p1.append(p2[0])
 p1.show()
-
-print(rSq(runExpansion(j, k), testfunct()))
